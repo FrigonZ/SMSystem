0d171f905edebff70f679d88ce8edcb3f8db70c0
Frigonz <820017235@qq.com>
2019-03-21 23:43:59 +0800

    Update IconButton.java

--- Project/src/main/frigon/IconButton.java
+++ Project/src/main/frigon/IconButton.java
@@ -10,6 +10,7 @@ public class IconButton extends JButton{
     private static final long serialVersionUID = 1L;
     ImageIcon preIcon;
     ImageIcon nowIcon;
+    ImageIcon aftIcon;
 
     public IconButton(ImageIcon pre,ImageIcon now){
         super();
@@ -31,5 +32,9 @@ public class IconButton extends JButton{
         setBorderPainted(false);
         setBorder(null);
         setContentAreaFilled(false);
+    }
+
+    public IconButton(ImageIcon pre,ImageIcon now,ImageIcon aft){
+        super();
     }
 }
